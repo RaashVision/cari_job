69f766d86966dac0412830a4bb3b28733e1f2872
Thiyraash David <thiyraash.david@optivolve.com>
2019-06-01 16:36:12 +0800

    addded login page and tabbed page

---
+++ flutter_app/lib/views/Drawerv2.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/views/job_list.dart';
+import 'package:flutter_app/views/top_tabbed_view.dart';
+
+class DrawerItem {
+  String title;
+  IconData icon;
+  DrawerItem(this.title, this.icon);
+}
+
+class HomePage extends StatefulWidget {
+  final drawerItems = [
+    new DrawerItem("Job Apply", Icons.add),
+    new DrawerItem("Fragment 2", Icons.local_pizza),
+    new DrawerItem("Fragment 3", Icons.info)
+  ];
+
+  @override
+  State<StatefulWidget> createState() {
+    return new HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage> {
+  int _selectedDrawerIndex = 0;
+
+  _getDrawerItemWidget(int pos) {
+    switch (pos) {
+      case 0:
+        return new TabBarCariKerja();
+      case 1:
+        return new ListPage();
+//      case 2:
+//        return new ThirdFragment();
+
+      default:
+        return new Text("Error");
+    }
+  }
+  
+  _onSelectItem(int index) {
+    setState(() => _selectedDrawerIndex = index);
+    Navigator.of(context).pop(); // close the drawer
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> drawerOptions = [];
+    for (var i = 0; i < widget.drawerItems.length; i++) {
+      var d = widget.drawerItems[i];
+      drawerOptions.add(
+        new ListTile(
+          leading: new Icon(d.icon),
+          title: new Text(d.title),
+          selected: i == _selectedDrawerIndex,
+          onTap: () => _onSelectItem(i),
+        )
+      );
+    }
+
+    return new Scaffold(
+      appBar: new AppBar(
+        backgroundColor: Colors.pink[800],
+        // here we display the title corresponding to the fragment
+        // you can instead choose to have a static title
+        title: new Text(widget.drawerItems[_selectedDrawerIndex].title),
+      ),
+      drawer: new Drawer(
+
+        child: new Column(
+          children: <Widget>[
+            new UserAccountsDrawerHeader(
+              otherAccountsPictures: <Widget>[
+                ClipOval(
+                  child: Image.asset('defaulticon.png',
+                    fit: BoxFit.cover,
+                    width: 40,
+                    height: 40,
+                  ),
+                ),
+              ],
+           currentAccountPicture: ClipOval(
+             child: Image.asset('ironman.jpg',
+               fit: BoxFit.cover,
+               width: 40,
+               height: 40,
+             ),
+           ),
+                accountName: new Text("Iron Man"), accountEmail: new Text("ironman@gmail.com"),),
+            new Column(children: drawerOptions)
+          ],
+        ),
+      ),
+      body: _getDrawerItemWidget(_selectedDrawerIndex),
+    );
+  }
+}
--- flutter_app/lib/views/top_tabbed_view.dart
+++ flutter_app/lib/views/top_tabbed_view.dart
@@ -6,88 +6,63 @@ class TabBarCariKerja extends StatefulWidget {
 }
 
 class _TabBarCariKerja extends State<TabBarCariKerja> {
-
-  Widget appBarTitle = new Text("AppBar Title");
-  Icon actionIcon = new Icon(Icons.search);
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: new ThemeData(
-        brightness: Brightness.light,
-        primaryColor: Colors.pink[800], //Changing this will change the color of the TabBar
-        accentColor: Colors.cyan[600],
-      ),
-      home: DefaultTabController(
+    return new Scaffold(
+
+      body: new DefaultTabController(
         length: 3,
-        child: Scaffold(
-
-          appBar: AppBar(
-            actions: <Widget>[
-              // action button
-              IconButton(
-                icon: new Stack(
-                  children: <Widget>[
-                    new Icon(Icons.notifications),
-                    new Positioned(
-                      right: 0,
-                      child: new Container(
-                        padding: EdgeInsets.all(1),
-                        decoration: new BoxDecoration(
-                          color: Colors.red,
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        constraints: BoxConstraints(
-                          minWidth: 12,
-                          minHeight: 12,
-                        ),
-                        child: new Text("1",
-                          style: new TextStyle(
-                            color: Colors.white,
-                            fontSize: 8,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-                // title: Text('Notifications'),
-              ),
-              IconButton(
-                icon: ClipOval(
-                  child: Image.asset(
-                    'flutterlogo.png',
-                    fit: BoxFit.cover,
-                  ),
-                ),
-
-                onPressed: () {
-
-                },
-              ),
-
-              // action button
-            ],
-            bottom: TabBar(
-
+        child: new Scaffold(
+          appBar: new AppBar(
+            actions: <Widget>[],
+            title: new TabBar(
               tabs: [
-                Tab(icon: Icon(Icons.home)),
-                Tab(icon: Icon(Icons.book)),
-                Tab(icon: Icon(Icons.bookmark)),
+                Tab(icon:ClipOval(
+                  child: Image.asset(
+                    'all.png',
+                    fit: BoxFit.cover,
+                    width: 30,
+                    height: 30,
+                  ),
+                )),
+                Tab(icon:
+                ClipOval(
+                  child: Image.asset(
+                    'linkedinicon.png',
+                    fit: BoxFit.cover,
+                    width: 30,
+                    height: 30,
+                  ),
+                )),
+                Tab(icon:ClipOval(
+                  child: Image.asset(
+                    'jobstreet.jpg',
+                    fit: BoxFit.cover,
+                    width: 30,
+                    height: 30,
+                  ),
+                ),),
+                Tab(icon:ClipOval(
+                  child: Image.asset(
+                    'indeed.jpg',
+                    fit: BoxFit.cover,
+                    width: 30,
+                    height: 30,
+                  ),
+                ),),
               ],
+              indicatorColor: Colors.white,
             ),
-            title: Text('Cari Kerja'),
           ),
-          body: TabBarView(
+          body: new TabBarView(
             children: [
-              Icon(Icons.directions_car),
-              Icon(Icons.directions_transit),
-              Icon(Icons.directions_bike),
+              new Icon(Icons.directions_car,size: 50.0,),
+              new Icon(Icons.directions_transit,size: 50.0,),
+              new Icon(Icons.directions_bike,size: 50.0,),
             ],
           ),
         ),
-      ),
+      )
     );
   }
 }
